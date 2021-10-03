f2b458d584605902ec09ccf86a73fcff517efa1e
abhisekmane98 <57854572+abhisekmane98@users.noreply.github.com>
2021-10-03 19:13:47 +0530

    Add files via upload

---
+++ 12_4/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << nullptr;
+    return 0;
+}
---
+++ 12_nullptr_type/cmake-build-debug/CMakeFiles/12_nullptr_type.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "12_nullptr_type.exe"
+  "12_nullptr_type.exe.manifest"
+  "12_nullptr_type.pdb"
+  "CMakeFiles/12_nullptr_type.dir/main.cpp.obj"
+  "lib12_nullptr_type.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12_nullptr_type.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
