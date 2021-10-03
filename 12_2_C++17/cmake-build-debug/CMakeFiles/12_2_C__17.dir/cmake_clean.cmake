bc32693cdf97cba1c5a04edcce56d91f0d11178f
abhisekmane98 <57854572+abhisekmane98@users.noreply.github.com>
2021-10-03 19:08:39 +0530

    Add files via upload

---
+++ 12_2_C++17/cmake-build-debug/CMakeFiles/12_2_C__17.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "12_2_C__17.exe"
+  "12_2_C__17.exe.manifest"
+  "12_2_C__17.pdb"
+  "CMakeFiles/12_2_C__17.dir/main.cpp.obj"
+  "lib12_2_C__17.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12_2_C__17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
